c09a1adea8d0af07963cd598ba5e38739ac54bfe
BERAT <berat.kaya@powercoders.org>
2024-10-01 12:08:06 +0200

    4.1.13 added

---
+++ week2/JSexercises/4.1.13.js
@@ -0,0 +1,14 @@
+/*
+Ex. 4.1.13 (*) - Looping an array
+Create an array of five numbers. Write a for loop to sum the numbers
+*/
+
+
+let myArray = [1,3,5,7,9];
+let sum=0;
+
+for (let i = 0; i < myArray.length; i++) {
+    sum += myArray[i]  ;
+
+}
+console.log(sum)
